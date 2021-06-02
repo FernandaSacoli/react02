c127c42daae986a35bd653ec51234b0968e19242
FernandaSacoli <fersacoli@gmail.com>
2021-06-02 12:11:59 +0000

    commit 1

---
+++ contador/src/components/ContadorCompleto.jsx
@@ -0,0 +1,53 @@
+import './ContadorCompleto.css'
+
+import React, { Component } from 'react'
+
+class Contador extends Component {
+    
+    state = {
+        numero: this.props.numeroInicial || 0,
+        passo: this.props.passo || 5,
+
+    }
+
+    inc= () => {
+        this.setState({
+            numero: this.state.numero + this.state.passo
+        })
+    }
+
+    dec = () => {
+        this.setState({
+            numero: this.state.numero - this.state.passo
+        })
+    }
+     setPasso= (event) => {
+         this.setState({
+             // o parametro event é o evento da tag
+             // osinal de + converte o value (que por padrão e string) para number
+             passo: event.target.value
+         })
+     }
+
+     render (){
+         return(
+             <div className="Contador">
+                 <h2> {this.state.numero} </h2>
+             
+        <div>
+             <label htmlFor="passoInput">Passo: </label>
+            <input
+            id="passoInput"
+            type="number"
+            value={this.state.passo}
+            onChange={this.setPasso}
+            />
+            </div>
+            <button onClick={this.inc}> + </button>
+            <button onClick={this.inc}> - </button>
+         </div>
+         )
+     }
+
+}
+export default Contador
